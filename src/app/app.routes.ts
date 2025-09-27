ffba13bceffe8dbe523a617e6ad0266e1e34f9f1
Zaza Gejadze <zazagdev@gmail.com>
2025-09-27 17:08:40 +0400

    Removed unsued files, set home as main and only file for now

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { LoginComponent } from './components/login/login.component';
-import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
-  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
-  { path: '**', redirectTo: '/login' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: '**', redirectTo: '/home' },
 ];
